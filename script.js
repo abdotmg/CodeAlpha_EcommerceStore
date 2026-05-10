150c19ce57ae2c7836303e155c4e9af4935a01d4
abd_osman8 <152434108+abdotmg@users.noreply.github.com>
2026-05-10 15:01:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://codealpha-ecommercestore-9y47.onrender.com";
 
 const productsContainer = document.getElementById("productsContainer");
 
